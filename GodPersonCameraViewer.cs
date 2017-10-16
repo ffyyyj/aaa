630a39e5017c4d3e4de0540f634dcd0f2e17e9ab
Liyangxin1994 <32827412+Liyangxin1994@users.noreply.github.com>
2017-10-16 15:03:22 +0800

    lyx
    lyx

--- GodPersonCameraViewer.cs
+++ GodPersonCameraViewer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+///liyangxin
 public class GodPersonCameraViewer : MonoBehaviour {
 	private Transform ViewPoint;
 	private Transform subViewPoint;
