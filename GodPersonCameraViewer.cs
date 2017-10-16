ed529aa55b127cc96d89a7e94ecf039d00f1daa6
striter <letsbattle@foxmail.com>
2017-10-16 15:08:01 +0800

    1
    ???

--- GodPersonCameraViewer.cs
+++ GodPersonCameraViewer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 ///luohui is sb
+/// //????
 public class GodPersonCameraViewer : MonoBehaviour {
 	private Transform ViewPoint;
 	private Transform subViewPoint;
